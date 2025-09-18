9352429390979561cc34dfb412dc3cc1ef2883f0
jeremydefreitas <jeremydefreitas@hotmail.com>
2025-09-18 01:26:56 -0700

    Time: 4 ms (73.37%) | Memory: 18 MB (46.03%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isBalanced(self, root):
+        
+
+        def dfs(root):
+            if not root:
+                return [True, 0]
+
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            if (left[0] and right[0]) and abs(left[1] - right[1]) <= 1 :
+                return [True, max(left[1], right[1]) + 1]
+            return [False, left + right]
+        
+        return dfs(root)[0]
+        
